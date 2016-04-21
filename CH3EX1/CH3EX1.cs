1a05c644e46d4001b66067cf313d79d48e870009
Michael Mckee <michael.mckee92@gmail.com>
2016-04-20 20:26:30 -0400

    Starting Chapter 3
    Very basic C# syntax, Console applications.

---
+++ CH3EX1/CH3EX1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Test the various forms of Main(); returning void, int. 
+// Include a batch file to run the program and capture the error code.
+    // This is located at the directory containing the compiled .exe
+namespace CH3EX1
+{
+    class CH3EX1
+    {
+        static int Main(string[] args)
+        {
+            // Display a simple message to the user.
+            Console.WriteLine("***** My First C# App (Not really) *****");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+
+            // Wait for the Enter key to be pressed before shutting down.
+            Console.ReadLine();
+
+            // Update the program to have a return type of int instead of void.
+            // Return an arbitrary error code.
+            return -1;
+        }
+    }
+}
---
+++ CH3EX2/CH3EX2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH3EX2
+{
+    class CH3EX2
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("**********************");
+
+            // Process any incoming args using a for loop:
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("Arg: {0}", args[i]);
+            }
+
+            Console.WriteLine("**********************");
+
+            // Process any incoming args using a foreach:
+            foreach (string arg in args)
+            {
+                Console.WriteLine("Arg {0}", arg);
+            }
+
+            Console.WriteLine("**********************");
+
+            // Process incoming args using the static GetCommandLineArgs() method from System.Environment:
+            // If using this method it is no longer necessary to declare string[] args in the Main method.
+            string[] theArgs = Environment.GetCommandLineArgs();
+            foreach (string arg in theArgs)
+            {
+                Console.WriteLine("Arg {0}", arg);
+            }
+
+            Console.WriteLine("**********************");
+            
+            // Helper method within the Program class.
+            ShowEnvironmentDetails();
+
+            Console.ReadLine();
+
+            return -1;
+        }
+
+        static void ShowEnvironmentDetails()
+        {
+            // Print out the drives on this machine, and other interesting details:
+            foreach (string drive in Environment.GetLogicalDrives())
+            {
+                Console.WriteLine("Drive: {0}", drive);
+            }
+
+            Console.WriteLine("OS: {0}", Environment.OSVersion);
+            Console.WriteLine("Number of processors: {0}", Environment.ProcessorCount);
+            Console.WriteLine(".NET version: {0}", Environment.Version);
+        }
+    }
+}
